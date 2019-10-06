1f4b3c9adbdbbd9a80bbc677e4470e31f3d349bc
Zhang Yuang <1328410180@qq.com>
2019-10-06 21:01:14 +0800

    Upload Models

--- demo.py
+++ demo.py
@@ -4,10 +4,10 @@ import numpy as np
 from model.resnet import resnet18sr
 from model.srcnn import srcnn
 
-model = srcnn()
-model.load_weights("model_srcnn.h5")
+model = resnet18sr()
+model.load_weights("model_resnet18sr.h5")
 
-im = cv2.imread("demo/xh.jpg")
+im = cv2.imread("test.jpg")
 # im = np.expand_dims(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY),-1)
 x = np.expand_dims(im, 0)/255
 x = x.transpose(3,1,2,0)
@@ -18,5 +18,5 @@ y = y.transpose(3,1,2,0)
 imy = np.uint8(np.clip(y[0], 0, 1)*255)
 demo = np.vstack([im, imy, imy-im+127])
 
-cv2.imwrite("demo/demo_srcnnh.jpg", demo)
-cv2.imwrite("demo/y_srcnnh.jpg", imy)
+cv2.imwrite("demo_resnet18srtest.jpg", demo)
+cv2.imwrite("y_resnet18srtest.jpg", imy)
